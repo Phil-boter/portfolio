6170f74e239eaa5edbdafb5bea597ed2b3d01d2b
Phil-boter <phildwd@gmail.com>
2021-04-19 21:12:13 +0200

    new portfolio

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const projects = require("./db/projects.json");
 
 const hb = require("express-handlebars");
 app.engine("handlebars", hb());
 app.set("view engine", "handlebars");
+
+app.use(express.static("./public"));
+app.options("*", cors()); // include before other routes
 
 app.use(
 	express.urlencoded({
@@ -11,20 +16,39 @@ app.use(
 	})
 );
 
-app.use(express.static("./public"));
+app.get("/", (req, res, next) => {
+	console.log("user is requesting main");
 
-app.get("/", (req, res) => {
-	console.log("user is requesting GET / route");
-	res.render("portfolio", {
+	res.render("mainpage", {
 		layout: "main",
+		msg: "This is CORS-enabled for all origins!",
 	});
 });
 
+app.get("/contact", (req, res) => {
+	console.log("user is requesting contact");
+	res.render("contact");
+});
+app.get("/about", (req, res) => {
+	console.log("user is rtequesting about");
+	res.render("about");
+});
+app.get("/projects", (req, res) => {
+	console.log("user is rtequesting projects");
+	console.log("req.params", req.params);
+	console.log("projetcs", projects);
+	// const selectedProject = projects.find((item) => item.directory == project);
+
+	// if (!selectedProject) {
+	// 	return res.sendStatus(404);
+	// } else {
+	res.render("projects", {
+		projects,
+		// selectedProject,
+	});
+	// }
+	// res.render("projects");
+});
 app.listen(process.env.PORT || 8080, () =>
-	console.log("petition server is listening at http://localhost:8080")
+	console.log("portfolio server is listening at http://localhost:8080")
 );
-// if (require.main == module) {
-// 	app.listen(process.env.PORT || 8080, () =>
-// 		console.log("portfolio server is listening")
-// 	);
-// }
--- public/script.js
+++ public/script.js
@@ -1,22 +1,129 @@
-// scroll name
-$(window).on("scroll", function () {
-	if (window.matchMedia("(max-width: 650px)").matches) {
-		$(".scrollHeader").css(
-			"font-size",
-			$(this).scrollTop() * 0.1 + 50 + "px"
-		);
-	} else {
-		$(".scrollHeader").css(
-			"font-size",
-			$(this).scrollTop() * 0.2 + 50 + "px"
-		);
-	}
+console.log("script.js is mounted");
+
+const options = {
+	root: null,
+	rootMargin: "-10%",
+	threshold: 0.0,
+};
+
+// ----------------    animation nav --------------
+
+let listItems = [...document.querySelectorAll("li")];
+
+let observer = new IntersectionObserver(showItem, options);
+
+function showItem(entries) {
+	entries.forEach((entry) => {
+		if (entry.isIntersecting) {
+			let letters = [...entry.target.querySelectorAll(`span`)];
+			letters.forEach((letter, idx) => {
+				setTimeout(() => {
+					letter.classList.add("active");
+				}, idx * 20);
+			});
+			entry.target.children[0].classList.add(`active`);
+		}
+	});
+}
+
+listItems.forEach((item) => {
+	let newString = "";
+	let itemText = item.children[0].innerText.split("");
+	itemText.map(
+		(letter) =>
+			(newString +=
+				letter == " "
+					? `<span class='gap'></span>`
+					: `<span>${letter}</span>`)
+	);
+	item.innerHTML = newString;
+	observer.observe(item);
 });
 
-//scroll portfolio
-$(window).on("scroll", function () {
-	$(".scrollPortfolio").css(
-		"font-size",
-		$(this).scrollTop() * 0.04 + 10 + "px"
+// ---------------  headlines --------------------
+
+let header = [...document.querySelectorAll("h1")];
+
+let headerObserver = new IntersectionObserver(showHeader, options);
+
+function showHeader(entries) {
+	// console.log("entries", entries);
+	entries.forEach((entry) => {
+		if (entry.isIntersecting) {
+			let letters = [...entry.target.querySelectorAll(`span`)];
+			letters.forEach((letter, idx) => {
+				setTimeout(() => {
+					letter.classList.add("active");
+				}, idx * 10);
+			});
+			entry.target.children[0].classList.add(`active`);
+		}
+	});
+}
+
+header.forEach((item) => {
+	// console.log("item", item);
+	let newHeader = "";
+	let headerText = item.children[0].innerText.split("");
+	headerText.map(
+		(letter) =>
+			(newHeader +=
+				letter == " "
+					? `<span class='gap'></span>`
+					: `<span>${letter}</span>`)
 	);
+	item.innerHTML = newHeader;
+	headerObserver.observe(item);
+});
+
+// ------------- change background ------------------
+
+function bgChanger() {
+	if (this.scrollY > this.innerHeight / 1.6) {
+		// this refers here to window
+		document.body.classList.add("bg-active");
+	} else {
+		document.body.classList.remove("bg-active");
+	}
+}
+window.addEventListener("scroll", bgChanger);
+
+// ----------navi-links -------------------------
+
+const target = document.getElementById("main-image");
+
+let observerImg = new IntersectionObserver(showImage);
+
+if (target) {
+	observerImg.observe(target);
+}
+
+function showImage(entries, observerImg) {
+	console.log("showImage", target);
+	entries.forEach((entry, idx) => {
+		if (entry.isIntersecting) {
+			console.log("is observing");
+			entry.target.classList.add("grow");
+		}
+	});
+}
+
+// ----------- project images ---------------
+
+let images = [...document.querySelectorAll(".article-image")];
+console.log(images);
+
+observerArticleImg = new IntersectionObserver(showArticleImage);
+
+function showArticleImage(entries) {
+	console.log(entries);
+	entries.forEach((entry) => {
+		if (entry.isIntersecting) {
+			entry.target.classList.add("shrink");
+		}
+	});
+}
+images.forEach((image) => {
+	console.log(image);
+	observerArticleImg.observe(image);
 });
